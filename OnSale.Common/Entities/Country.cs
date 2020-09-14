a89b1b723e2bd436f0a62bd35123da69e848dc16
gerardo lanza <glanza007@gmail.com>
2020-09-14 11:08:47 -0600

    Countries CRUD

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+       [MaxLength(50, ErrorMessage = "The Field {0} must contains less than {1} characters.")]
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
