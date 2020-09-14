d3a610ec36ac07d126ed9e21627b7cffab0a7ef2
gerardo lanza <glanza007@gmail.com>
2020-09-14 15:31:36 -0600

    CRUD department and countries

--- OnSale.Common/Entities/City.cs
+++ OnSale.Common/Entities/City.cs
@@ -1,20 +1,19 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
+﻿using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OnSale.Common.Entities
 {
-    public class Country
+    public class City
     {
         public int Id { get; set; }
+
         [MaxLength(50, ErrorMessage = "The Field {0} must contains less than {1} characters.")]
         [Required]
         public string Name { get; set; }
 
-        public ICollection<Department> Departments { get; set; }
-
-        [DisplayName("Departments Number")]
-        public int DepartmentsNumber => Departments == null ? 0 : Departments.Count;
-
+        [JsonIgnore]
+        [NotMapped]
+        public int IdDepartment { get; set; }
     }
 }
---
+++ OnSale.Common/Entities/Department.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The Field {0} must contains less than {1} characters.")]
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<City> Cities { get; set; }
+
+        [DisplayName("Cities Number")]
+        public int CitiesNumber => Cities == null ? 0 : Cities.Count;
+
+        [JsonIgnore]
+        [NotMapped]
+        public int IdCountry { get; set; }
+    }
+}
