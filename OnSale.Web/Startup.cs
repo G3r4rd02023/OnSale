875f6836c154e605da07ce8a2e2d6b2d334ce36a
gerardo lanza <glanza007@gmail.com>
2020-09-15 15:26:51 -0600

    Add Images para un solo producto

--- OnSale.Web/Startup.cs
+++ OnSale.Web/Startup.cs
@@ -41,6 +41,7 @@ namespace OnSale.Web
 
             services.AddScoped<IBlobHelper, BlobHelper>();
             services.AddScoped<IConverterHelper, ConverterHelper>();
+            services.AddScoped<ICombosHelper, CombosHelper>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         
     }
